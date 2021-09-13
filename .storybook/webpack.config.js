d7916668ea6db823081e0d04d5b7bda63b2fdb71
Evan Horsley <emhorsley@intxlog.com>
2021-09-13 10:58:45 -0400

    Almsot there just having scss issues

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,6 +1,51 @@
+// module.exports = {
+//   module: {
+//     rules: [
+//       {
+//         test: /\.scss$/,
+//         loaders: [
+//           require.resolve('style-loader'),
+//           {
+//             loader: require.resolve('css-loader'),
+//             options: {
+//               importLoaders: 1,
+//               modules: true,
+//               localIdentName: '[name]__[local]___[hash:base64:5]',
+//             }
+//           },
+//           require.resolve('sass-loader')
+//         ]
+//       },
+//       {
+//         test: /\.(?:jpe?g|gif|png|wav|mp3)$/,
+//         loader: 'file-loader?name=[name]-[hash].[ext]'
+//       },
+//     ]
+//   }
+// }
+const path = require('path')
+const nodeExternals = require('webpack-node-externals');
+
 module.exports = {
+  watch: true,
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  mode: 'production',
+  entry: path.resolve(__dirname, 'src/lib/index.js'),
+  output: {
+    path: path.resolve(__dirname, './dist/lib'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs'
+  },
+  externals: [nodeExternals()],
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      },
       {
         test: /\.scss$/,
         loaders: [
@@ -15,11 +60,18 @@ module.exports = {
           },
           require.resolve('sass-loader')
         ]
-      },
-      {
-        test: /\.(?:jpe?g|gif|png|wav|mp3)$/,
-        loader: 'file-loader?name=[name]-[hash].[ext]'
-      },
+      }
+      // {
+      //   test: /\.s[ac]ss$/i,
+      //   use: [
+      //     // Creates `style` nodes from JS strings
+      //     "style-loader",
+      //     // Translates CSS into CommonJS
+      //     "css-loader",
+      //     // Compiles Sass to CSS
+      //     "sass-loader",
+      //   ],
+      // },
     ]
   }
 }
